6f480d0d72e6246d66d7906f65930efa86860638
Rhazarian <Rhazarian@ya.ru>
2024-03-16 19:24:42 +0100

    new ApplyUnitBehaviorAbilityBehavior

--- src/engine/behaviour/ability/apply-unit-behavior.ts
+++ src/engine/behaviour/ability/apply-unit-behavior.ts
@@ -1,30 +1,64 @@
 import { AbilityBehavior } from "../ability"
 import { Ability } from "../../internal/ability"
-import { UnitBehavior, UnitBehaviorConstructor } from "../unit"
+import { UnitBehavior } from "../unit"
 import { Unit } from "../../internal/unit"
+import { MutableKeys } from "../../../utility/types"
+import { AbilityDependentValue, AbilityField, AbilityLevelField } from "../../object-field/ability"
 
-export class ApplyUnitBehaviorAbilityBehavior<Args extends any[]> extends AbilityBehavior {
-    private readonly createUnitBehavior: (unit: Unit) => UnitBehavior
+type UnitBehaviorParameters<T extends UnitBehavior> = Partial<{
+    [K in MutableKeys<T> & KeysOfType<T, boolean | number | string>]: T[K] extends
+        | boolean
+        | number
+        | string
+        ? AbilityDependentValue<T[K]>
+        : never
+}>
 
-    private unitBehavior?: UnitBehavior
+export class ApplyUnitBehaviorAbilityBehavior<T extends UnitBehavior> extends AbilityBehavior {
+    private readonly keys?: (keyof UnitBehaviorParameters<T>)[]
+
+    private unitBehavior?: T
 
     public constructor(
         ability: Ability,
-        unitBehaviorConstructor: UnitBehaviorConstructor<Args>,
-        ...args: Args
+        private readonly unitBehaviorConstructor: new (unit: Unit) => T,
+        private readonly parameters?: UnitBehaviorParameters<T>
     ) {
         super(ability)
-        this.createUnitBehavior = (unit) => {
-            return new unitBehaviorConstructor(unit, ...args)
+        if (parameters != undefined) {
+            this.keys = Object.keys(parameters).sort() as (keyof UnitBehaviorParameters<T>)[]
+        }
+    }
+
+    public update(): void {
+        const unitBehavior = this.unitBehavior
+        const parameters = this.parameters
+        const keys = this.keys
+        if (unitBehavior != undefined && parameters != undefined && keys != undefined) {
+            for (const key of keys) {
+                const value = parameters[key]
+                if (value != undefined) {
+                    unitBehavior[key] = this.resolveCurrentAbilityDependentValue(value)
+                }
+            }
         }
     }
 
     public override onUnitGainAbility(unit: Unit): void {
         this.unitBehavior?.destroy()
-        this.unitBehavior = this.createUnitBehavior(unit)
+        this.unitBehavior = new this.unitBehaviorConstructor(unit)
+        this.update()
     }
 
     public override onUnitLoseAbility(): void {
         this.unitBehavior?.destroy()
     }
 }
+
+AbilityField.valueChangeEvent.addListener((ability) => {
+    ApplyUnitBehaviorAbilityBehavior.forAll(ability, "update")
+})
+
+AbilityLevelField.valueChangeEvent.addListener((ability) => {
+    ApplyUnitBehaviorAbilityBehavior.forAll(ability, "update")
+})
