c59dd14515dd3ee32c35b2682bad714eef69e17f
Rhazarian <Rhazarian@ya.ru>
2024-05-02 01:46:28 +0200

    randomXY

--- src/engine/random.ts
+++ src/engine/random.ts
@@ -1,7 +1,11 @@
-import { MAXIMUM_INTEGER } from "../math"
+import { MAXIMUM_INTEGER, PI } from "../math"
 
 const getRandomInt = GetRandomInt
 const getRandomReal = GetRandomReal
+
+const cos = math.cos
+const sin = math.sin
+const sqrt = math.sqrt
 
 export const randomAngle = (): number => getRandomReal(0, 360)
 
@@ -16,3 +20,13 @@ export const randomFloat: {
     (lowerBound: number, upperBound: number): number
 } = (m?: number, n?: number): number =>
     n != undefined ? getRandomReal(m!, n) : getRandomReal(0, m ?? MAXIMUM_INTEGER)
+
+export const randomXY = (
+    centerX: number,
+    centerY: number,
+    range: number
+): LuaMultiReturn<[x: number, y: number]> => {
+    const r = range * sqrt(getRandomReal(0, 1))
+    const t = getRandomReal(0, 1) * 2 * PI
+    return $multi(centerX + r * cos(t), centerY + r * sin(t))
+}
