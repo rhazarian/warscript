d5f6f386cca2bdc3c5c6f74405bacd2d61a03fa8
Rhazarian <Rhazarian@ya.ru>
2025-05-11 02:35:10 +0200

    few SoundPresetName

--- src/engine/object-data/auxiliary/sound-preset-name.ts
+++ src/engine/object-data/auxiliary/sound-preset-name.ts
@@ -6,4 +6,10 @@ export const enum SoundPresetName {
     ABOMINATION_WAR_CRY = "AbominationWarcry",
     // Combat
     AXE_MEDIUM_CHOP_WOOD = "AxeMediumChopWood",
+    // Animation
+    IMPALE = "Impale",
+    IMPALE_HIT = "ImpaleHit",
+    IMPALE_LAND = "ImpaleLand",
+    // Abilities
+    IMPALE_CAST = "ImpaleCast",
 }
