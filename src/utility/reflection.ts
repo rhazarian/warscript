c0560641f2a4e99341b6666f9986f3d460f5b269
Admiral <Rhazarian@ya.ru>
2025-06-08 14:10:25 +0200

    try fix

--- src/utility/reflection.ts
+++ src/utility/reflection.ts
@@ -7,29 +7,33 @@ interface AbstractConstructorTSTLInternals extends AbstractConstructor {
 
 /** @internal For use by internal systems only. */
 export const implementReadonlyNumberIndexSupplier = <
-    T extends AbstractConstructor & Record<PropertyKey, any>
+    T extends AbstractConstructor & Record<PropertyKey, any>,
 >(
     clazz: T,
     supplier: (
-        key: T extends Record<infer K, any> ? Exclude<K, string> : never
-    ) => T extends Record<infer K, any> ? T[Exclude<K, string>] : never
+        key: T extends Record<infer K, any> ? Exclude<K, string> : never,
+    ) => T extends Record<infer K, any> ? T[Exclude<K, string>] : never,
 ): void => {
     const metatable = checkNotNull(getmetatable(clazz))
-    const originalIndex = checkNotNull(metatable.__index)
+    const originalIndex = checkNotNull(rawget(metatable, "__index"))
     const memoizedValueByKey = new LuaMap<number, any>()
-    metatable.__index = setmetatable(memoizedValueByKey, {
-        __index: function (this: any, key: any): any {
-            if (typeof key == "number") {
-                const value = supplier(key as any)
-                memoizedValueByKey.set(key, value)
-                return value
-            }
-            if (typeof originalIndex == "function") {
-                return (originalIndex as any)(this, key)
-            }
-            return (originalIndex as any)[key]
-        },
-    })
+    rawset(
+        metatable,
+        "__index",
+        setmetatable(memoizedValueByKey, {
+            __index: function (this: any, key: any): any {
+                if (typeof key == "number") {
+                    const value = supplier(key as any)
+                    memoizedValueByKey.set(key, value)
+                    return value
+                }
+                if (typeof originalIndex == "function") {
+                    return (originalIndex as any)(this, key)
+                }
+                return (originalIndex as any)[key]
+            },
+        }),
+    )
 }
 
 export const getClass = <T extends object>(object: T): Constructor<T> | undefined => {
@@ -37,7 +41,7 @@ export const getClass = <T extends object>(object: T): Constructor<T> | undefine
 }
 
 export const getSuperclass = <T extends object>(
-    clazz: AbstractConstructor<T>
+    clazz: AbstractConstructor<T>,
 ): AbstractConstructor<T> | undefined => {
     return (clazz as AbstractConstructorTSTLInternals).____super
 }
