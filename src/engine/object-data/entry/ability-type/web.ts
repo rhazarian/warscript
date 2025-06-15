9ee77063368518643efeca29f8378830fd1da296
Admiral <Rhazarian@ya.ru>
2025-06-15 15:44:30 +0200

    WebAbilityType

---
+++ src/engine/object-data/entry/ability-type/web.ts
@@ -0,0 +1,32 @@
+import { AbilityType, AbilityTypeId } from "../ability-type"
+import { ObjectDataEntryLevelFieldValueSupplier } from "../../entry"
+
+export class WebAbilityType extends AbilityType {
+    public static override readonly BASE_ID = fourCC("Aweb") as AbilityTypeId
+
+    public get airUnitLoweringDuration(): number[] {
+        return this.getNumberLevelField("Ens1")
+    }
+
+    public set airUnitLoweringDuration(
+        airUnitLoweringDuration: ObjectDataEntryLevelFieldValueSupplier<number>,
+    ) {
+        this.setNumberLevelField("Ens1", airUnitLoweringDuration)
+    }
+
+    public get airUnitHeight(): number[] {
+        return this.getNumberLevelField("Ens2")
+    }
+
+    public set airUnitHeight(airUnitHeight: ObjectDataEntryLevelFieldValueSupplier<number>) {
+        this.setNumberLevelField("Ens2", airUnitHeight)
+    }
+
+    public get meleeAttackRange(): number[] {
+        return this.getNumberLevelField("Ens3")
+    }
+
+    public set meleeAttackRange(meleeAttackRange: ObjectDataEntryLevelFieldValueSupplier<number>) {
+        this.setNumberLevelField("Ens3", meleeAttackRange)
+    }
+}
