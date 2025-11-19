aa7c93a1ef388033dc8ff9717f82f43cdb6747a1
Admiral <Rhazarian@ya.ru>
2025-11-19 03:15:29 +0100

    fly heigt as unit field

---
+++ src/engine/internal/unit/fly-height.ts
@@ -0,0 +1,16 @@
+import { Unit } from "../unit"
+import { FLY_HEIGHT_UNIT_FLOAT_FIELD } from "../../standard/fields/unit"
+
+declare module "../unit" {
+    interface Unit {
+        readonly flyHeight: number
+    }
+}
+Object.defineProperty(Unit.prototype, "flyHeight", {
+    get(this: Unit): number {
+        return FLY_HEIGHT_UNIT_FLOAT_FIELD.getValue(this)
+    },
+    set(this: Unit, value: number) {
+        FLY_HEIGHT_UNIT_FLOAT_FIELD.setValue(this, value)
+    },
+})
--- src/engine/standard/fields/unit.ts
+++ src/engine/standard/fields/unit.ts
@@ -1,5 +1,11 @@
-import { UnitClassificationsField, UnitPropulsionWindowField } from "../../object-field/unit"
+import {
+    UnitClassificationsField,
+    UnitFloatField,
+    UnitPropulsionWindowField,
+} from "../../object-field/unit"
 
 export const PROPULSION_WINDOW_UNIT_FLOAT_FIELD = UnitPropulsionWindowField.create(fourCC("urpw"))
 
 export const UNIT_CLASSIFICATIONS_FIELD = UnitClassificationsField.create(fourCC("utyp"))
+
+export const FLY_HEIGHT_UNIT_FLOAT_FIELD = UnitFloatField.create(fourCC("ufyh"))
