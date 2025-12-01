1d3eae2671d9f8666d56527eab53af5f1b017afc
Admiral <Rhazarian@ya.ru>
2025-12-01 02:26:52 +0100

    manaRegenerationRateIncreaseFactor ???

--- src/engine/standard/fields/unit.ts
+++ src/engine/standard/fields/unit.ts
@@ -1,9 +1,9 @@
 import {
-    UnitClassificationsField,
+    UnitClassificationsField, UnitFloatField,
     UnitFlyHeightField,
     UnitHealthRegenerationTypeField,
     UnitPropulsionWindowField,
-    UnitScalingValueField,
+    UnitScalingValueField
 } from "../../object-field/unit"
 
 export const PROPULSION_WINDOW_UNIT_FLOAT_FIELD = UnitPropulsionWindowField.create(fourCC("urpw"))
@@ -13,6 +13,10 @@ export const UNIT_CLASSIFICATIONS_FIELD = UnitClassificationsField.create(fourCC
 export const FLY_HEIGHT_UNIT_FLOAT_FIELD = UnitFlyHeightField.create(fourCC("ufyh"))
 
 export const SCALING_VALUE_UNIT_FLOAT_FIELD = UnitScalingValueField.create(fourCC("usca"))
+
+export const HEALTH_REGENERATION_RATE_UNIT_FLOAT_FIELD = UnitFloatField.create(fourCC("uhpr"))
+
+export const MANA_REGENERATION_RATE_UNIT_FLOAT_FIELD = UnitFloatField.create(fourCC("umpr"))
 
 export const UNIT_HEALTH_REGENERATION_TYPE_FIELD = UnitHealthRegenerationTypeField.create(
     fourCC("uhrt"),
